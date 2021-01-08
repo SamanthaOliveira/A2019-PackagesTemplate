d8fd7df027bca7562f24cda3b65ef31349085f86
SamanthaOliveira <samantha_230308@hotmail.com>
2021-01-08 14:09:26 -0300

    TEMPLATE

--- src/main/java/com/automationanywhere/commands/tests/MiscTests.java
+++ src/main/java/com/automationanywhere/commands/tests/MiscTests.java
@@ -12,14 +12,15 @@ public class MiscTests {
     public static void main(String[] args){
         //test 1
         SimpleAction command = new SimpleAction();
-        StringValue d = command.action("bren");
-        assertEquals("Oh hi there, bren",d.toString());
+        StringValue d = command.action("Samantha");
+        assertEquals("Oh hi there, Samantha",d.toString());
 
         // test 2
         SimpleAction4Rest command4 = new SimpleAction4Rest();
-        StringValue d4 = command4.action("https://viacep.com.br/ws/01001000/json/");
+        StringValue d4 = command4.action("https://viacep.com.br/ws/09725160/json/");
         System.out.println(d4.toString());
 
+        //test 3
         SimpleAction4Rest2 command5 = new SimpleAction4Rest2();
         StringValue d5 = command5.action("https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/sp");
         System.out.println(d5.toString());
